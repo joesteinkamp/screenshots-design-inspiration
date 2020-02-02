f91059afdbc39e79b4a4be239f3f71c1a159c070
Joe Steinkamp <joe@joesteinkamp.com>
2020-02-01 23:33:24 -0600

    updated filter toggles

--- assets/js/toggle-visibility.js
+++ assets/js/toggle-visibility.js
@@ -1,7 +1,19 @@
-function toggleVisibility(toggleClass) {
+function filterToggle(toggleClass) {
+    this.classList.toggle('active');
+
     var x = document.getElementsByClassName(toggleClass);
 
     for (var i = 0; i < x.length; i++) {
         x[i].classList.toggle('hidden');
     }
 }
+
+
+function filterImages(toggleClass) {
+    var x = document.getElementsByClassName(toggleClass);
+
+    for (var i = 0; i < x.length; i++) {
+        x[i].classList.toggle('hidden');
+    }
+}
+
