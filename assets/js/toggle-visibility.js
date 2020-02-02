f2b2b991ea97a47dcbe3e8aeb546690f58983492
Joe Steinkamp <joe@joesteinkamp.com>
2020-02-01 23:09:26 -0600

    Update toggle-visibility.js

--- assets/js/toggle-visibility.js
+++ assets/js/toggle-visibility.js
@@ -1,5 +1,5 @@
-function toggleClass(className) {
-    var x = document.getElementsByClassName(className);
+function toggleClass(toggleClass) {
+    var x = document.getElementsByClassName(toggleClass);
 
     for (var i = 0; i < x.length; i++) {
         x[i].classList.toggle('hidden');
